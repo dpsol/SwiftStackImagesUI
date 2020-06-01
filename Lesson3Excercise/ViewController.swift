59f70d991bb61e35f09771cc8b5bb6033fcdcd5d
Daniel Pérez Solís <dpsol2008@gmail.com>
2020-06-01 18:33:12 -0500

    Initial Commit

---
+++ Lesson3Excercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson3Excercise
+//
+//  Created by Daniel Pérez Solís on 01/06/20.
+//  Copyright © 2020 Artnetsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
